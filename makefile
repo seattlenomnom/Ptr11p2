27c083106edb1f71494bdf646d5b9a3284a7f111
seattlenomnom <mark.crapser@gmail.com>
2023-07-02 22:20:05 -0700

    first commit

---
+++ makefile
@@ -0,0 +1,4 @@
+# Makefile for ptr11p2
+#
+ptr11p2: ptr11p2.c
+	gcc -g ptr11p2.c -o ./Build/ptr11p2
---
+++ ptr11p2.c
@@ -0,0 +1,46 @@
+/* source file for ptr11p2.c
+ *
+ * ptr11p2 demonstrtes the function insertEntry(). This function takes as
+ * arguments a pointer to a struct entry (the listHead) and another pointer
+ * to a struct entry after which the new entry is to be placed.
+ *
+ */
+
+/* includes */
+
+
+
+
+
+/* defines */
+
+
+
+
+/* structs */
+
+
+
+
+/* function declarations */
+
+
+
+
+
+/* main */
+
+int main(int argc, char *argv[]){
+
+
+
+
+
+
+
+    return(0);
+}
+
+
+
+/* funtion definitions */
