19753a643162db45fdb6d69540439cec2ab628a7
seattlenomnom <mark.crapser@gmail.com>
2023-07-02 22:41:03 -0700

    Added comments to top of ptr11p2.c

--- ptr11p2.c
+++ ptr11p2.c
@@ -4,6 +4,13 @@
  * arguments a pointer to a struct entry (the listHead) and another pointer
  * to a struct entry after which the new entry is to be placed.
  *
+ * Sunday July 2nd
+ *   I'm very tired. There is confusion in my mind about how the linked list
+ *   and insertEntry() are supposed to work together. Simplify the fisrt
+ *   versions. Think about how any singly linked list would work with a
+ *   function like insertEntry(). How does the book present the problem?
+ *
+ *
 */
 
 /* includes */
